5e8a72b8d9ca286c06211154e74bec688e963d2d
Kazuki Ohara <kazuki.ohara@gmail.com>
2021-04-03 00:34:15 +0900

    osbook_day05d

--- kernel/font.hpp
+++ kernel/font.hpp
@@ -6,3 +6,5 @@
 
 void WriteAscii(PixelWriter& writer, int x, int y, char c,
                 const PixelColor& color);
+void WriteString(PixelWriter& writer, int x, int y, const char* s,
+                 const PixelColor& color);
